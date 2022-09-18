6411c1af032623908fa6606d8ddbc9dab819d4c3
an-nix <antoni.nicolas@gmail.com>
2022-09-19 00:16:17 +0200

    client

---
+++ src/network/clientManager.cpp
@@ -0,0 +1,22 @@
+#include "clientManager.h"
+
+
+ClientManager::ClientManager()
+{
+    
+}
+
+ClientManager::~ClientManager()
+{
+    
+}
+
+
+void ClientManager::fctInit()
+{
+    // client type
+    // coupling version
+    // 
+
+
+}
